5a0fe7069fd503ffb09c4ffc862655ab6440a4e0
Joel Jogy <joeljogy07@gmail.com>
2024-08-26 15:57:00 -0400

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,6 +5,6 @@ def error_prog(var1):
     var3 = "World!"
     print("Type of var1",type(var1))
     print("Type of var2",type(var2))
+    print(len(var2 + var3))
     var = var1 + var2 + var3
     return (var)
-print(error_prog(10))
