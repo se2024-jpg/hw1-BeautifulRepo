92b60cc850c6c19bb475c37f8151d39b2c6361bd
Girish G N <girishgn12@gmail.com>
2024-08-26 15:53:42 -0400

    Update test_my_program.py

--- test_my_program.py
+++ test_my_program.py
@@ -1,5 +1,5 @@
 import pytest
-from my_program import error_prog
+from myfile import error_prog
 
 def test_passing_case():
     # This test should pass because the integer is converted to a string explicitly
