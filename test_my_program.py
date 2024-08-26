5e7d02e4b2d6aea78fac58209a865ead4394f273
Girish G N <girishgn12@gmail.com>
2024-08-26 15:53:15 -0400

    Creating pytest testcases

---
+++ test_my_program.py
@@ -0,0 +1,11 @@
+import pytest
+from my_program import error_prog
+
+def test_passing_case():
+    # This test should pass because the integer is converted to a string explicitly
+    assert error_prog(str(10)) == "10HelloWorld!"
+
+def test_failing_case():
+    # This test should fail as it will raise a TypeError when trying to concatenate int to str directly
+    with pytest.raises(TypeError):
+        error_prog(10)
