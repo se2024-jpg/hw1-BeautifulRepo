1d3faef2b1afb664b8f5af8c0beca2d6d272bd0a
prav29 <38226613+prav29@users.noreply.github.com>
2024-08-29 00:38:54 -0400

    Update test_my_program.py

--- test_my_program.py
+++ test_my_program.py
@@ -6,6 +6,3 @@ def test_passing_case():
     
 def test_failing_case():
     print(error_prog(10))
-    # This test should fail as it will raise a TypeError when trying to concatenate int to str directly
-    # with pytest.raises(TypeError):
-    #     print (error_prog(10))
