45d8a7d4c3a569b0e8ead655a67e4e10cf370b48
Joel Jogy <joeljogy07@gmail.com>
2024-08-29 02:06:01 -0400

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -2,7 +2,7 @@ print("Homework 1 - ToDo #5")
 print("Team - ggopala4, jjgeorg2, pvasire2 \n")
 def error_prog(var1):
     var2 = "Hello"
-    var3 = "World"
+    var3 = "World!"
     print("Type of var1",type(var1))
     print("Type of var2",type(var2))
     var = var1 + var2 + var3
