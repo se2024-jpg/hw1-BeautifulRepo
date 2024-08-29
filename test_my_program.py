e9c121ef446567faba3f08ba738b6cc129db96d9
prav29 <38226613+prav29@users.noreply.github.com>
2024-08-29 00:37:24 -0400

    Update test_my_program.py

--- test_my_program.py
+++ test_my_program.py
@@ -1,10 +1,6 @@
 import pytest
 from myfile import error_prog
 
-def test_passing_case():
-    # This test should pass because the integer is converted to a string explicitly
-    print (error_prog(str(10)))
-
 def test_failing_case():
     # This test should fail as it will raise a TypeError when trying to concatenate int to str directly
     with pytest.raises(TypeError):
