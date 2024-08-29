ab0fb646b2551d1ae74f9df98082986d3509f9cd
prav29 <38226613+prav29@users.noreply.github.com>
2024-08-29 00:38:36 -0400

    Update test_my_program.py

--- test_my_program.py
+++ test_my_program.py
@@ -1,6 +1,9 @@
 import pytest
 from myfile import error_prog
 
+def test_passing_case():
+    print(error_prog(str(10)))
+    
 def test_failing_case():
     print(error_prog(10))
     # This test should fail as it will raise a TypeError when trying to concatenate int to str directly
