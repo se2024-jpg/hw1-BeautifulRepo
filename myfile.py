c4e95a8f00e85e95a57437ebd270eef39e1a2289
Joel Jogy <joeljogy07@gmail.com>
2024-08-29 01:30:55 -0400

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,5 +1,5 @@
 print("Homework 1 - ToDo #5")
-print("Team - ggopala4, jjgeorg2, pvasire2\n")
+print("Team - ggopala4, jjgeorg2, pvasire2 \n")
 def error_prog(var1):
     var2 = "Hello"
     var3 = "World!"
